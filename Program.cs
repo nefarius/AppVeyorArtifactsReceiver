1dfaf562b431eca505796a998a6ad91a698a5b62
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-01-11 20:25:43 +0100

    Deduplicated code

--- Program.cs
+++ Program.cs
@@ -2,39 +2,17 @@ global using FastEndpoints;
 
 using AppVeyorArtifactsReceiver;
 
-using Serilog;
-using Serilog.Core;
+using Nefarius.Utilities.AspNetCore;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder();
+WebApplicationBuilder builder = WebApplication.CreateBuilder().Setup();
+
 builder.Services.AddFastEndpoints();
 builder.Services.AddHttpClient();
 
-
 builder.Services.Configure<ServiceConfig>(builder.Configuration.GetSection(nameof(ServiceConfig)));
 
-#region Logging
 
-Logger logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration)
-    .Enrich.FromLogContext()
-    .CreateLogger();
-
-// logger instance used by non-DI-code
-Log.Logger = logger;
-
-builder.Host.UseSerilog(logger);
-
-builder.Services.AddLogging(b =>
-{
-    b.SetMinimumLevel(LogLevel.Information);
-    b.AddSerilog(logger, true);
-});
-
-builder.Services.AddSingleton(new LoggerFactory().AddSerilog(logger));
-
-#endregion
-
-WebApplication app = builder.Build();
+WebApplication app = builder.Build().Setup();
 
 app.UseAuthorization();
 app.UseFastEndpoints();
