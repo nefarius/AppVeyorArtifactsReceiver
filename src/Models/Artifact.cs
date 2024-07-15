e600527e97649d0040a28d1335b16b9aa96bc2a4
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-07-16 01:15:28 +0200

    Split file

---
+++ src/Models/Artifact.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace AppVeyorArtifactsReceiver.Models;
+
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+public sealed class Artifact
+{
+    [JsonPropertyName("fileName")]
+    public string FileName { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("size")]
+    public int Size { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+}
--- src/Models/WebhookRequest.cs
+++ src/Models/WebhookRequest.cs
@@ -3,27 +3,6 @@ using System.Text.Json.Serialization;
 
 namespace AppVeyorArtifactsReceiver.Models;
 
-[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
-[SuppressMessage("ReSharper", "UnusedMember.Global")]
-[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
-public sealed class Artifact
-{
-    [JsonPropertyName("fileName")]
-    public string FileName { get; set; }
-
-    [JsonPropertyName("name")]
-    public string Name { get; set; }
-
-    [JsonPropertyName("type")]
-    public string Type { get; set; }
-
-    [JsonPropertyName("size")]
-    public int Size { get; set; }
-
-    [JsonPropertyName("url")]
-    public string Url { get; set; }
-}
-
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
 [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
