c3d8b0295f87cb49255b13e99c9d637cfe3b724d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-08-15 16:20:21 +0200

    Updated deps

--- src/Endpoints/WebhooksEndpoint.cs
+++ src/Endpoints/WebhooksEndpoint.cs
@@ -8,7 +8,7 @@ namespace AppVeyorArtifactsReceiver.Endpoints;
 /// <summary>
 ///     Endpoint listening for incoming webhook requests.
 /// </summary>
-internal sealed class WebhooksEndpoint(IOptions<ServiceConfig> serviceConfig) : Endpoint<WebhookRequest>
+internal sealed class WebhooksEndpoint(IOptions<ServiceConfig> serviceConfig, ILogger<WebhooksEndpoint> logger) : Endpoint<WebhookRequest>
 {
     public override void Configure()
     {
@@ -18,13 +18,15 @@ internal sealed class WebhooksEndpoint(IOptions<ServiceConfig> serviceConfig) : 
 
     public override async Task HandleAsync(WebhookRequest req, CancellationToken ct)
     {
+        logger.LogDebug("Received webhook request for {Id}", req.Id);
+        
         if (!serviceConfig.Value.Webhooks.Any(kvp => Equals(Guid.Parse(kvp.Key), req.Id)))
         {
-            await SendNotFoundAsync(ct);
+            await Send.NotFoundAsync(ct);
             return;
         }
 
         await PublishAsync(req, Mode.WaitForNone, ct);
-        await SendOkAsync(ct);
+        await Send.OkAsync(ct);
     }
 }
